ddec1a9b22d67993a2bd3171f7bdda3ae9e2c80f
moksha <moksha0224@berkeley.edu>
2023-01-18 17:39:49 +0800

    accomplish lru-k part of p1

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -14,16 +14,97 @@
 
 namespace bustub {
 
-LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
+LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {
+  BUSTUB_ASSERT(k != 0, "elicit value of k!");
+  record_.assign(num_frames, std::vector<int>());  // initialize the num_frames of empty vector to record K's access.
+}
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+  if (lru_replacer_.empty()) {
+    return false;
+  }
+  *frame_id = lru_replacer_.back().first;
+  record_[*frame_id].clear();  // remove the frame's access history.
+  lru_replacer_.pop_back();  // evict the frame with largest backward k-distance.
+  hash_.erase(*frame_id);
+  curr_size_--;
+  return true;
+}
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id) {}
+void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
+  BUSTUB_ASSERT(frame_id <= (int)replacer_size_, "frame id is invalid.");
+  auto& timestamp_arr = record_[frame_id];
+  timestamp_arr.push_back(current_timestamp_);  // update elements of record array.
+  auto it = hash_.find(frame_id);
+  if (it != hash_.end()) {  // exist. lrureplacer needed to be adjusted.
+    int curr_k_distance = INF;
+    if (timestamp_arr.size() >= k_) {
+      curr_k_distance = timestamp_arr.back() - timestamp_arr.at(timestamp_arr.size()-k_);
+      it->second->second = curr_k_distance;  // update k_distance.
+    }
+    auto l = lru_replacer_.begin();
+    while (l != lru_replacer_.end()) {
+      if (l->second == curr_k_distance && timestamp_arr.at(0) < record_[l->first].at(0)) {  // multiple +INF
+        std::advance(l, 1);
+      } else if (l->second < curr_k_distance) {
+        std::advance(l, 1);
+      } else {
+        break;
+      }
+    }
+    lru_replacer_.splice(l, lru_replacer_, it->second);
+  }
+  ++current_timestamp_;  // every access with incrementing time stamp.
+}
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+  // BUSTUB_ASSERT(frame_id <= (int)replacer_size_, "frame id is invalid.");
+  BUSTUB_ASSERT(!record_[frame_id].empty(), "frame id is invalid.");
+  auto timestamp_arr = record_[frame_id];
+  auto it = hash_.find(frame_id);
+  if (set_evictable && it == hash_.end()) {  // non-evictable to evictable
+    // insert suitable location in list.
+    int curr_k_distance = INF;
+    if (timestamp_arr.size() >= k_) {
+      curr_k_distance = timestamp_arr.back() - timestamp_arr.at(timestamp_arr.size()-k_);
+    }
+    if (lru_replacer_.empty()) {
+      lru_replacer_.push_front({frame_id, curr_k_distance});
+      hash_.insert({frame_id, lru_replacer_.begin()});  // update iterator.
+    } else {
+      auto l = lru_replacer_.begin();
+      while (l != lru_replacer_.end()) {
+        if (l->second == curr_k_distance && timestamp_arr.at(0) < record_[l->first].at(0)) {  // multiple +INF
+          std::advance(l, 1);
+        } else if (l->second < curr_k_distance) {
+          std::advance(l, 1);
+        } else {
+          break;
+        }
+      }
+      lru_replacer_.insert(l, {frame_id, curr_k_distance});  // insert before l.
+      hash_.insert({frame_id, std::prev(l, 1)});  // update the current frame's iterator in hash table.
+    }
+    curr_size_++;
+  } else if (!set_evictable && it != hash_.end()) {  // evictable to non-evictable
+    lru_replacer_.remove({frame_id, it->second->second});
+    hash_.erase(frame_id);
+    curr_size_--;
+  }
+}
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+void LRUKReplacer::Remove(frame_id_t frame_id) {
+  // BUSTUB_ASSERT(hash_, "not be able to remove non-evictable frame.");
+  auto it = hash_.find(frame_id);
+  if (it == hash_.end()) {  // frame_id not found.
+    return;
+  }
+  record_[frame_id].clear();  // remove the frame's access history.
+  lru_replacer_.remove({frame_id, it->second->second});
+  hash_.erase(frame_id);
+  curr_size_--;
+}
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
+auto LRUKReplacer::Size() -> size_t { return curr_size_; }
 
 }  // namespace bustub
--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -17,9 +17,12 @@
 #include <mutex>  // NOLINT
 #include <unordered_map>
 #include <vector>
+#include <limits.h>  // use INT_MAX
 
 #include "common/config.h"
 #include "common/macros.h"
+
+#define INF INT_MAX
 
 namespace bustub {
 
@@ -135,11 +138,14 @@ class LRUKReplacer {
  private:
   // TODO(student): implement me! You can replace these member variables as you like.
   // Remove maybe_unused if you start using them.
-  [[maybe_unused]] size_t current_timestamp_{0};
-  [[maybe_unused]] size_t curr_size_{0};
-  [[maybe_unused]] size_t replacer_size_;
-  [[maybe_unused]] size_t k_;
+  size_t current_timestamp_{0};
+  size_t curr_size_{0};
+  size_t replacer_size_;
+  size_t k_;
   std::mutex latch_;
+  std::list<std::pair<int, int>> lru_replacer_;  // <frame_id, k_distance>
+  std::vector<std::vector<int>> record_;  // record the K nums of access history.
+  std::unordered_map<int, std::list<std::pair<int, int>>::iterator> hash_;
 };
 
 }  // namespace bustub
